f1b25fe7d88bfd3692153afb7b5b1cb9eb95d65f
Anna Urbanik <urbanik.ania@gmail.com>
2022-11-19 16:04:52 +0100

    Add 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -42,7 +42,10 @@
   const targetArticle = document.querySelector(articleSelector)
   console.log(targetArticle)
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active')
+
   }
   
   const links = document.querySelectorAll('.titles a');
